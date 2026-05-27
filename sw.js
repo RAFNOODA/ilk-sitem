036a564d24380512ac6da7756b192cdd36c0199b
RAFNOODA <musabhde@gmail.com>
2026-05-27 17:24:12 +0300

    tam surum pwa

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('Uygulama kuruldu!');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Şimdilik boş, ama PWA şartı olduğu için ekliyoruz
+});
